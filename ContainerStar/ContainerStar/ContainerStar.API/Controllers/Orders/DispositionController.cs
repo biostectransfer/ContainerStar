2b296f80f4530b12200133ed0c75f960314cca70
Yury Spiridonov <spiridonov.yury@gmail.com>
2015-07-09 22:08:13 +0200

    home page v 1.0

---
+++ ContainerStar/ContainerStar/ContainerStar.API/Controllers/Orders/DispositionController.cs
@@ -0,0 +1,93 @@
+using ContainerStar.API.Models;
+using ContainerStar.API.Security;
+using ContainerStar.Contracts.Enums;
+using ContainerStar.Contracts.Managers;
+using ContainerStar.Contracts.Services;
+using CoreBase;
+using System;
+using System.Web.Http;
+
+namespace ContainerStar.API.Controllers
+{
+
+    /// <summary>
+    ///     Controller for offers
+    /// </summary>
+    [AuthorizeByPermissions(PermissionTypes = new[] { Permissions.Orders })]
+    public partial class DispositionController : ApiController
+    {
+        private readonly IOrdersManager manager;
+        private readonly IUniqueNumberProvider numberProvider;
+
+        public DispositionController(IOrdersManager manager, IUniqueNumberProvider numberProvider)
+        {
+            this.manager = manager;
+            this.numberProvider = numberProvider;
+        }
+
+        public IHttpActionResult Post(int containerTypeId, string name)
+        {
+            return Ok(new [] { 
+            
+                    new {
+                        title = "All Day Event",
+                        start = "2015-02-01"
+                    },
+                    new {
+                        title = "Long Event",
+                        start = "2015-02-07",
+                        //end = "2015-02-10"
+                    },
+                    new {
+                        //id =  999,
+                        title = "Repeating Event",
+                        start = "2015-02-09T16:00:00"
+                    },
+                    new {
+                        //id = 999,
+                        title = "Repeating Event",
+                        start = "2015-02-16T16:00:00"
+                    },
+                    new {
+                        title = "Conference",
+                        start = "2015-02-11",
+                        //end = "2015-02-13"
+                    },
+                    new {
+                        title = "Meeting",
+                        start = "2015-02-12T10:30:00",
+                        //end = "2015-02-12T12:30:00"
+                    },
+                    new {
+                        title = "Lunch",
+                        start = "2015-02-12T12:00:00"
+                    },
+                    new {
+                        title = "Meeting",
+                        start = "2015-02-12T14:30:00"
+                    },
+                    new {
+                        title = "Happy Hour",
+                        start = "2015-02-12T17:30:00"
+                    },
+                    new {
+                        title = "Dinner",
+                        start = "2015-02-12T20:00:00"
+                    },
+                    new {
+                        title = "Birthday Party",
+                        start = "2015-02-13T07:00:00"
+                    },
+                    new {
+                        title = "Click for Google",
+                        //url = "http://google.com/",
+                        start = "2015-02-28"
+                    },
+                    new {
+                        title = String.Format("{0} {1}", containerTypeId, name),
+                        start = "2015-07-09T07:00:00"
+                    },
+            });
+        }
+    }
+}
--- ContainerStar/ContainerStar/ContainerStarWeb/web/views/home/home.js
+++ ContainerStar/ContainerStar/ContainerStarWeb/web/views/home/home.js
@@ -1,15 +1,73 @@
 ﻿define([
-	'base/base-view'
-], function (BaseView) {
+	'base/base-view',
+    'calendar',
+    'calendarLanguages',
+    'mixins/kendo-widget-form',
+	'mixins/kendo-validator-form',
+	'mixins/bound-form'
+], function (BaseView, Calendar, CalendarLanguages, KendoWidgetFormMixin, KendoValidatorFormMixin, BoundForm) {
 	'use strict';
-
+    
 	var view = BaseView.extend({
+
+	    bindings: function () {
+
+	        var self = this;
+	        var result = {
+	            
+	        };
+
+
+	        return result;
+	    },
+
+	    initialize: function () {
+
+	        view.__super__.initialize.apply(this, arguments);
+
+	        this.model = new Backbone.Model();
+	    },
+
 		render: function () {
-			view.__super__.render.apply(this, arguments);
+		    view.__super__.render.apply(this, arguments);
+
+		    var self = this;
+
+		    setTimeout(function () {
+		        self.$el.find('#calendar').fullCalendar({
+		            header: {
+		                left: 'prev,next today',
+		                center: 'title',
+		                right: ''
+		            },
+		            editable: false,
+		            lang: "de",
+		            eventLimit: true, // allow "more" link when too many events
+
+		            events: {
+		                url: Application.apiUrl + 'disposition/?containerTypeId=777&name=ololo',
+		                type: 'POST',
+		                //data: {
+		                //    containerTypeId: 1,
+		                //    name: 'somethingelse'
+		                //},
+		                error: function (e) {
+		                    debugger;
+		                    //alert('there was an error while fetching events!');
+		                },
+		                //color: 'yellow',   // a non-ajax option
+		                //textColor: 'black' // a non-ajax option
+		            }
+		        });
+		    }, 0);
 
 			return this;
 		}
 	});
-
+    
+	view.mixin(BoundForm);
+	view.mixin(KendoValidatorFormMixin);
+	view.mixin(KendoWidgetFormMixin);
+    
 	return view;
 });
